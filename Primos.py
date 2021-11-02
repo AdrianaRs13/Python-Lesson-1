652fb96557018ef51f921893a274e8d40d94c339
AdrianaRs13 <adriana.rosas@enroutesystems.com>
2021-11-02 17:02:24 -0600

    Primos

---
+++ Primos.py
@@ -0,0 +1,14 @@
+var=0
+l=False
+while (var<101):
+    if var%2!=0:
+        if var%3!=0:
+            if var%5!=0:
+                if var%7!=0:
+                    if var%11!=0:
+                        if var%13!=0:
+                            l=True
+                            print (var)
+
+
+
--- hw.py
+++ hw.py
@@ -1,1 +1,8 @@
-print ('Hello World')
+var = 0
+
+while val<=100:
+    if (val%2==0):
+        print (val)
+    val+=1
+
+
