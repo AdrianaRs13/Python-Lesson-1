14d4408310b5b606bfa92e7e52af9f3f165698e6
AdrianaRs13 <adriana.rosas@enroutesystems.com>
2021-11-02 17:13:17 -0600

    Primos

--- Primos.py
+++ Primos.py
@@ -1,6 +1,6 @@
 var=0
 l=False
-while (var<101):
+for var in range(101):
     if var%2!=0:
         if var%3!=0:
             if var%5!=0:
@@ -9,6 +9,7 @@ while (var<101):
                         if var%13!=0:
                             l=True
                             print (var)
+    
 
 
 
